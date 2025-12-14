452d3931a499202999abc33e6338e94b0ee94748
Fernando-Cortes-Renteria <fercorren@gmail.com>
2025-12-13 22:10:09 -0600

    updated main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # I import and configure pandas display options
 pd.set_option('display.max_columns', None)   # Show all columns
-pd.set_option('display.width', 200)          # Wider output
+pd.set_option('display.width', 120)          # Wider output
 pd.set_option("display.precision", 2)
 pd.set_option('display.float_format', '{:,.2f}'.format)
 
@@ -17,6 +17,7 @@ def main():
 
     # merge into a single dataframe
     florida_df = pd.concat([broward, miami], ignore_index=True)
+    florida_df.rename(columns={'parcel_id': 'ParcelID'}, inplace=True)
 
     ''' filter for residential properties (DOR_UC codes 1-4):
     1 - Single Family Residential
@@ -27,11 +28,15 @@ def main():
 
     florida_residential = florida_df[florida_df['property_use'] <= 4]
     tamarac_permits = pd.read_csv(r'./Tamarac/Tamarac_cleaned.csv')
+    tamarac_cleaned = tamarac_permits[['ParcelID', 'WorkType', 'Contractor', 'Status']]
     hallandale_beach_permits = pd.read_csv('Hallandale_beach.csv')
     hallandale_cleaned = hallandale_beach_permits.rename(columns={
         'Main Parcel': 'ParcelID',
         'Type': 'WorkType',
         'Company Name': 'Contractor'
+    })[['ParcelID', 'WorkType', 'Contractor', 'Status', 'Applied Date']]
+
+    permits = pd.concat([tamarac_cleaned, hallandale_cleaned], ignore_index=True)
 
     '''
     Tamarac has the following variables:
@@ -47,13 +52,30 @@ def main():
     Address is not necessary thanks to florida_residential having situs_address
     Contractor is very useful
     WorkType/Type is very useful and similar
+
+    Tamarac does not have any date information, while Hallandale Beach does.
+    It may be necessary to add date information to Tamarac from another source later on.
     '''
 
-    merged_df = pd.merge(tamarac_permits, florida_residential, left_on='ParcelID', right_on='parcel_id', how='inner')
+    merged_df = pd.merge(permits, hallandale_cleaned, left_on='ParcelID', right_on='ParcelID', how='inner')
 
     # print the parcels with multiple permits
-    # print(merged_df["parcel_id"].value_counts()[merged_df["parcel_id"].value_counts() > 1])
+    duplicated_permits = merged_df["ParcelID"].value_counts()[merged_df["ParcelID"].value_counts() > 1]
+    
+    final_df = merged_df[merged_df['ParcelID'].isin(duplicated_permits.index)]
+    final_df['Applied Date_y'] = pd.to_datetime(final_df['Applied Date_y'], format='%m/%d/%Y')
+    final_df = final_df.sort_values(by='Applied Date_y')
+    final_df['Time Since Last Permit'] = final_df.groupby('ParcelID')['Applied Date_y'].diff().dt.days
 
+    avg_time = final_df.loc[final_df['Time Since Last Permit'].notna() & (final_df['Time Since Last Permit'] != 0),
+                            'Time Since Last Permit'].mean()
 
+    print(f"Average time since last permit (days): {avg_time:.2f}")
+
+    print(final_df.dtypes)
+
+    miamidade_residential_parcels = florida_residential[florida_residential['county'] == 'Miami-Dade']
+
+    
 
 main()
